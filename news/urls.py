de6c00e7f6be2482cfd2bd14c4bc248db8817d2a
root <adhikariratna52@gmail.com>
2017-01-28 09:47:07 +0545

    template inheritance

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$',views.articles_list,name='articles_list')
+    url(r'^$',views.articles_list,name='articles_list'),
+    url(r'^feeds/', views.feeds_list, name='feeds_list')
 ]

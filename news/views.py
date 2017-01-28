0bd13983d6f93c3e0522c7941f596b6bd03fea03
root <adhikariratna52@gmail.com>
2017-01-28 22:55:31 +0545

    did solved articles of a certain feed only

--- news/views.py
+++ news/views.py
@@ -11,6 +11,11 @@ def articles_list(request):
     rows = [articles[x:x+2] for x in range(0,len(articles),2)]
     return render(request,'news/articles_list.html',{'rows':rows})
 
+def articles_list_of_a_feed(request,a_feed):
+    articles = Article.objects.filter(feed__title=a_feed)
+    rows = [articles[x:x+2] for x in range(0,len(articles),2)]
+    return render(request,'news/articles_list.html',{'rows':rows})
+
 def feeds_list(request):
     feeds = Feed.objects.all()
     return render(request,'news/feeds_list.html',{'feeds':feeds})
@@ -20,24 +25,27 @@ def new_feed(request):
         form = FeedForm(request.POST)
         if form.is_valid():
             feed = form.save(commit=False)
-            feedData = feedparser.parse(feed.url)
-            feed.title = feedData.feed.title
-            feed.save()
-            for entry in feedData.entries:
-                try:
-                    article = Article()
-                    article.title = entry.title
-                    article.feed = feed
-                    article.description = entry.description
-                    article.url = entry.link
+            #check whether the url is already existing or not
+            existingFeed = Feed.objects.filter(url=feed.url)
+            if(len(existingFeed)==0):
+                feedData = feedparser.parse(feed.url)
+                feed.title = feedData.feed.title
+                feed.save()
+                for entry in feedData.entries:
+                    try:
+                        article = Article()
+                        article.title = entry.title
+                        article.feed = feed
+                        article.description = entry.description
+                        article.url = entry.link
 
-                    d = datetime.datetime(*(entry.published_parsed[0:6]))
-                    dateString = d.strftime('%Y-%m-%d %H:%M:%S')
-                    article.published_date = dateString
+                        d = datetime.datetime(*(entry.published_parsed[0:6]))
+                        dateString = d.strftime('%Y-%m-%d %H:%M:%S')
+                        article.published_date = dateString
 
-                    article.save()
-                except:
-                    pass
+                        article.save()
+                    except:
+                        pass
             return redirect('feeds_list')
     else:
         form = FeedForm()
